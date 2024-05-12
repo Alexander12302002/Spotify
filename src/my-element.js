d3b35f0276a3c31357eb1305be144e396db0e714
Aki_1130 <106930173+Alexander12302002@users.noreply.github.com>
2024-05-12 14:23:16 -0500

    feat: :sparkles: Completion of the card design

--- src/my-element.js
+++ src/my-element.js
@@ -1,36 +1,105 @@
 import { LitElement, css, html} from "lit";
 
 export class MyElement extends LitElement{ 
-    static styles = css`p { color: blue }`;
-
     static properties = {
+      name_song: {type: String}
     };
   
     constructor() {
       super();
-      this.imagen = "/src/assets/image22.svg"
+      this.imagen = "/src/assets/image22.jpg"
+      this.name_song = `Magazine`
+      this.name_artist = "$uicideBoys"
+      this.time = "3:14"
+      this.date_exit = 2016
     }
   
     render() {
       return html`
         <div class="card">
-            <div class="cards">           
-            <div class="aling"></div>
-            <div class="img"><img src="${this.imagen}"></div>
-            <div class="Titule_and_artis"></div>
-            <div class="minutes_and_date"></div>
+            <div class="cards">
+            <div class="cards_info">
+              <div class="aling">
+                    <button  type="button"> <box-icon name='menu'></box-icon></button>
+              </div>
+              <div class="img"><img src="${this.imagen}"></div>
+              <div class="Titule_and_artis">
+                    <h3>${this.name_song}</h3>
+                    <h5>${this.name_artist}</h5>
+              </div>
+            </div>           
+              <div class="minutes_and_date">
+                    <h4>${this.time}</h4>
+                    <h5>${this.date_exit}</h5>
+              </div>
             </div>
+            
         </div>
       `;
     }
 
     static styles = css`
-        .cards{
-            background: black;
-            wi
-        }
-    `
+    .card {
+      width: 300px;
+      margin: 10px;
+      padding: 0;
+  }
 
+  .img img {
+      border-radius: 50px;
+      width: 50px;
+      height: 50px; 
+  }
+
+  .cards{
+      background: white;
+      height: 50px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      overflow: hidden; 
+      margin-bottom: 10px
+  }
+
+  .cards h3{
+    margin: 0;
+  }
+
+  .cards h5{
+    margin: 0;
+    color: rgb(124, 124, 124);
+  }
+
+  .cards h4{
+    margin: 0;
+  }
+
+  .cards button{
+    background: none;
+    border: none;
+  }
+
+  .card button:hover{
+    cursor: pointer;
+  }
+
+  .cards .cards_info{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .cards .cards_info .img{
+    height: 50px;
+    width: 50px
+  }
+
+  .cards .cards_info .img img{
+    height: 50px;
+    width: 50px;
+    object-fit: cover;
+  }
+    `
     _guardar(e){
         let cancion = this.name(e.target.value)
         return(cancion)
