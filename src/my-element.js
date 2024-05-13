73a625815f8905b2738e9056369bab2dbd9bf560
Aki_1130 <106930173+Alexander12302002@users.noreply.github.com>
2024-05-13 11:19:22 -0500

    feat: :recycle: rebilling using the spotify API

--- src/my-element.js
+++ src/my-element.js
@@ -1,54 +1,70 @@
 import { LitElement, css, html} from "lit";
+import { map } from "lit/directives/map.js";
 
 export class MyElement extends LitElement{ 
     static properties = {
-      name_song: {type: String}
+      songs: {type: Array}
     };
+
   
     constructor() {
       super();
-      this.imagen = "/src/assets/image22.jpg"
-      this.name_song = `Magazine`
-      this.name_artist = "$uicideBoys"
-      this.time = "3:14"
-      this.date_exit = 2016
+      this.song = []
+      this.loadSong();
+    }
+
+    async loadSong() {
+      const url = 'https://spotify23.p.rapidapi.com/artist_singles/?id=2w9zwq3AktTeYYMuhMjju8&offset=0&limit=10';
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': '6dd82b226cmsh0ef7d9c5c6c533cp117684jsn444cf17bc680',
+          'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+      };
+    
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        this.songs = result.data.artist.discography.singles.items;
+        this.requestUpdate();
+    } catch (error) {
+        console.error('Error fetching songs:', error);
+    }
     }
   
     render() {
       return html`
+      <div>
+        ${Array.isArray(this.songs) && this.songs.length > 0 ? 
+        this.songs.map(song => html`
         <div class="card">
-            <div class="cards">
+        <div class="cards">
             <div class="cards_info">
-              <div class="aling">
-                    <button  type="button"> <box-icon name='menu'></box-icon></button>
-              </div>
-              <div class="img"><img src="${this.imagen}"></div>
-              <div class="Titule_and_artis">
-                    <h3>${this.name_song}</h3>
-                    <h5>${this.name_artist}</h5>
-              </div>
-            </div>           
-              <div class="minutes_and_date">
-                    <h4>${this.time}</h4>
-                    <h5>${this.date_exit}</h5>
-              </div>
+                <div class="aling"><button  type="button"> <box-icon name='menu'></box-icon></button></div>
+                <div class="img"><img src="${song.releases.items[0].coverArt.sources[0].url}"></div>
+                <div class="Titule_and_artis">
+                    <h3>${song.releases.items[0].name}</h3>
+                </div>
+                </div>
+                <div class="minutes_and_date">
+                <h5>${song.releases.items[0].date.year}</h5>
             </div>
-            
         </div>
-      `;
+    </div>  `)
+
+     : 
+      html`<p>No hay canciones disponibles</p>`
     }
+    </div>
+`;
+}
 
     static styles = css`
     .card {
       width: 300px;
       margin: 10px;
       padding: 0;
-  }
-
-  .img img {
-      border-radius: 50px;
-      width: 50px;
-      height: 50px; 
   }
 
   .cards{
@@ -62,16 +78,13 @@ export class MyElement extends LitElement{
   }
 
   .cards h3{
-    margin: 0;
+    margin: 5px;
+    font-size: 15px ;
   }
 
   .cards h5{
     margin: 0;
     color: rgb(124, 124, 124);
-  }
-
-  .cards h4{
-    margin: 0;
   }
 
   .cards button{
@@ -98,6 +111,10 @@ export class MyElement extends LitElement{
     height: 50px;
     width: 50px;
     object-fit: cover;
+  }
+
+  .cards .minutes_and_date{
+    padding-right: 10px
   }
     `
     _guardar(e){
