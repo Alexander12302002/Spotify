034db17c4d7e9ffcc86d0d915fdc5769c24df094
Alexander12302002 <106930173+Alexander12302002@users.noreply.github.com>
2024-05-15 16:14:52 -0500

    feat: :construction: Now playing under construction

--- src/my-element.js
+++ src/my-element.js
@@ -1,7 +1,15 @@
 import { LitElement, css, html} from "lit";
-import { map } from "lit/directives/map.js";
+import infinitLogo from "/mastercard-line.svg"
+import userLogo from "/Ellipse 13.svg"
+import compassLogo from "/Frame 24.svg"
+import earphonesLogo from "/Frame 19.svg"
+import heartLogo from "/Frame 23.svg"
+import bookLogo from "/Frame 20.svg"
+import gearLogo from "/Frame 22.svg"
+import exitLogo from "/Frame 26.svg"
+import { newMusic } from "./components/newMusic";
 
-export class MyElement extends LitElement{ 
+export class myTrack extends LitElement{ 
     static properties = {
       songs: {type: Array}
     };
@@ -14,11 +22,11 @@ export class MyElement extends LitElement{
     }
 
     async loadSong() {
-      const url = 'https://spotify23.p.rapidapi.com/artist_singles/?id=2w9zwq3AktTeYYMuhMjju8&offset=0&limit=10';
+      const url = 'https://spotify23.p.rapidapi.com/artist_singles/?id=2w9zwq3AktTeYYMuhMjju8&offset=0&limit=30';
       const options = {
         method: 'GET',
         headers: {
-          'X-RapidAPI-Key': '6dd82b226cmsh0ef7d9c5c6c533cp117684jsn444cf17bc680',
+          'X-RapidAPI-Key': '29392637f6msh22f35e67f4b3080p17fbe7jsn0806c5a433fb',
           'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
         }
       };
@@ -62,7 +70,6 @@ export class MyElement extends LitElement{
 
     static styles = css`
     .card {
-      width: 300px;
       margin: 10px;
       padding: 0;
   }
@@ -117,9 +124,365 @@ export class MyElement extends LitElement{
     padding-right: 10px
   }
     `
-    _guardar(e){
-        let cancion = this.name(e.target.value)
-        return(cancion)
-    }
 }   
-customElements.define('my-element', MyElement);
+customElements.define('my-track', myTrack);
+
+export class MyElement extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class = "container">
+        <nav class = "leftBar">
+          <my-left-bar></my-left-bar>
+        </nav>
+        <div class = "leftSection">
+          <my-left-section></my-left-section>
+        </div>
+        <div class = "middleSection">
+          <my-middle-section></my-middle-section>
+        </div>
+        <div class = "rightSection">
+          <my-right-section></my-right-section>
+        </div>
+      </div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      .container{
+        display: grid;
+        grid-template-columns: .2fr 1.5fr 2fr 1.5fr;
+        height: 98vh;
+        border: 1px solid black;
+        gap: .5em; 
+      }
+      .leftBar{
+        grid-column:1/2;
+        /*background: red;*/
+      }
+      .leftSection{
+        grid-column:2/3;
+        /*background: blue;*/
+      }
+      .middleSection{
+        grid-column: 3/4;
+        /*background: yellow;*/
+      }
+      .rightSection{
+        grid-column: 4/5;
+        /*background: violet;*/
+      }
+
+      @media (max-width: 750px) {
+        .container{
+          grid-template-columns: 1fr;
+        }
+        .middleSection{
+          grid-column:1/2;
+          background: black;
+        }
+        .leftBar{
+          display: none;
+        }
+        .leftSection{
+          display: none;
+        }
+        .rightSection{
+          display: none;
+        }
+      }
+    `
+  }
+}
+customElements.define('my-element', MyElement)
+
+export class MyLeftBar extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class="leftBar__user">
+        <a href="#">
+          <img src=${infinitLogo}>
+        </a>
+        <a href="#">
+          <img src=${userLogo}>
+        </a>
+      </div>
+      <hr>
+      <div class="leftBar__options">
+        <a href="#">
+          <img src=${compassLogo}>
+        </a>
+        <a href="#">
+          <img src=${earphonesLogo}>
+        </a>
+        <a href="#">
+          <img src=${heartLogo}>
+        </a>
+        <a href="#">
+          <img src=${bookLogo}>
+        </a>
+      </div>
+      <div class="leftBar__exit">
+        <a href="#">
+          <img src=${gearLogo}>
+        </a>
+        <a href="#">
+          <img src=${exitLogo}>
+        </a>
+      </div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      :host{
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        /*justify-content: center;*/
+        background: gray;
+        height: 98vh;
+      }
+      img{
+        width: 2.5em;
+        height: auto;
+      }
+      .leftBar__user{
+        display: flex;
+        flex-direction: column;
+        margin: 2em 0;
+      }
+      .leftBar__options{
+        display: flex;
+        flex-direction: column;
+        margin-top: 2em;
+        gap: 1em
+      }
+      .leftBar__exit{
+        position: absolute;
+        bottom: 5em;
+        display: flex;
+        flex-direction: column;
+      }
+    `
+  }
+}
+customElements.define('my-left-bar', MyLeftBar); 
+
+export class MyLeftSection extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class="leftSection__title"><h1>Discover New music</h1></div>
+      <div class="leftSection__topChart"><new-music class="new-music"></new-music></div>
+      <div class="leftSection__mayLike"><my-track></my-track></div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      :host{
+        display: grid;
+        grid-template-areas: 
+        "title",
+        "topChart",
+        "mayLike";
+        grid-template-rows: repeat(3, 1fr);
+        height: 98vh;
+        gap: 1em;
+        /*padding: 0 .5em;*/
+        
+      }
+      .leftSection__title{
+        margin-top: 1em;
+        grid-areas: "title";
+        background: white;
+        height: 10vh;
+      }
+      .leftSection__topChart{
+        grid-areas: "topChart";
+        background: white;
+        height: 50vh;
+        display: flex;
+        align-content: center;
+        flex-flow: column wrap;
+        justify-content: center;
+      }
+      .leftSection__topChart .new-music{
+        height: 100%;
+      }
+      .leftSection__mayLike{
+        grid-areas: "mayLike";
+        background: white;
+        height: 30vh;
+        overflow: scroll
+      }
+      .leftSection__mayLike::-webkit-scrollbar {
+        display: none;
+      }
+    `
+  }
+}
+customElements.define('my-left-section', MyLeftSection); 
+
+export class MyMiddleSection extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class = middleSection__container>
+        <div class = "middleSection__title"></div>
+        <div class = "middleSection__image"></div>
+        <div class = "middleSection__songTitle"></div>
+        <div class = "middleSection__barDuration"></div>
+        <div class = "middleSection__bottons"></div>
+        <div class = "middleSection__barVolume"></div>
+        <div class = "middleSection__deviceReference"></div>
+      </div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      .middleSection__container{
+        display:grid;
+        grid-template-rows: .2fr, 2fr, .35fr, .15fr, .45fr, .12fr, .2fr;
+        gap: .5em;
+        height: 96vh;
+        padding: 1em 0;
+      }
+      .middleSection__title{
+        grid-rows: 1/2;
+        background: white;
+        max-height: 40px;
+      }
+      .middleSection__image{
+        grid-rows: 2/3;
+        background: white;
+        min-height: 300px;
+      }
+      .middleSection__songTitle{
+        grid-rows: 3/4;
+        background: white;
+        
+      }
+      .middleSection__barDuration{
+        grid-rows: 4/5;
+        background: white;
+        max-height: 25px;
+        display: flex;
+        align-items:center;
+        justify-content: center;
+      }
+      .middleSection__bottons{
+        grid-rows: 5/6;
+        background: white;
+        min-height: 30px;
+      }
+      .middleSection__barVolume{
+        grid-rows: 6/7;
+        background: white;
+        max-height: 20px;
+      }
+      .middleSection__deviceReference{
+        grid-rows: 7/8;
+        background: white;
+        max-height: 40px;
+      }
+    `
+  }
+}
+customElements.define('my-middle-section', MyMiddleSection); 
+
+export class MyRightSection extends LitElement {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+      <div class = rightSection__container>
+        <div class = "rightSection__title"><h1>Track list</h1></div>
+        <div class = "rightSection__tools"><box-icon name='repost' color='#908e8e' ></box-icon> <box-icon name='shuffle' color='#908e8e'></box-icon></div>
+        <div class = "rightSection__playingNext"><h3>Playing next<h3></div>
+        <div class = "rightSection__Songs"><my-track></my-track></div>
+      </div>
+    `
+  }
+
+  static get styles() {
+    return css`
+      .rightSection__container{
+        display: grid;
+        grid-template-areas: 
+        "title",
+        "tools",
+        "playingNext",
+        "Song";
+        grid-template-rows: repeat(4, 1fr);
+        height: 96vh;
+        padding: 1em 0;
+        gap: .1em;
+      }
+      .rightSection__title{
+        grid-areas: "title";
+        background: white;
+        height: 5vh;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+      }
+      .rightSection__title h1{
+        color: #333333;
+        font-size: 2.5em;
+        margin-left: 10px;
+      }
+      .rightSection__tools{
+        grid-areas: "tools";
+        background: white;
+        height: 5vh;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+      }
+
+      .rightSection__playingNext{
+        grid-areas: "playingNext";
+        background: white;
+        height: 5vh;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+      }
+      .rightSection__Songs{
+        grid-areas: "Song";
+        background: white;
+        height: 80vh;
+        overflow: scroll
+      }
+
+      .rightSection__Songs::-webkit-scrollbar {
+        display: none;
+      }
+    `
+  }
+}
+customElements.define('my-right-section', MyRightSection); 
